06f10086919fcb496abfa4a1eaabc125acca2ec4
h_ohta <h_ohta@ertl.jp>
2016-10-03 22:42:11 +0900

    Devided nvidia driver scripts into two files

---
+++ blacklist-nouveau.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get remove nvidia* && sudo apt-get autoremove
+
+echo -e "blacklist nouveau\nblacklist lbm-nouveau\noptions nouveau modeset=0\nalias nouveau off\nalias lbm-nouveau off" | sudo tee /etc/modprobe.d/blacklist-nouveau.conf > /dev/null
+
+echo options nouveau modeset=0 | sudo tee /etc/modprobe.d/nouveau-kms.conf > /dev/null
+
+sudo update-initramfs -u
