56f87f952c92d2b36eea9607ccf6090f62cabce6
h_ohta <h_ohta@ertl.jp>
2016-10-03 22:54:03 +0900

    Add two install patterns, with opengl or without opengl

--- install-nvidia-driver.sh
+++ install-nvidia-driver.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-readonly FILENAME="NVIDIA-Linux-x86_64-367.35.run"
-readonly FILEURL="http://jp.download.nvidia.com/XFree86/Linux-x86_64/367.35/${FILENAME}"
 
-sudo stop lightdm
+readonly VERSION="367.35"
+readonly FILENAME="NVIDIA-Linux-x86_64-$VERSION.run"
+readonly FILEURL="http://jp.download.nvidia.com/XFree86/Linux-x86_64/$VERSION/$FILENAME"
 
-cd $HOME/Downloads
-wget -nc $FILEURL
-chmod +x $FILENAME
-sudo sh $FILENAME
+InstallProcess() {
+
+  echo sudo stop lightdm
+
+  echo cd $HOME/Downloads
+  echo wget -nc $FILEURL
+  echo chmod +x $FILENAME
+  echo sudo sh $FILENAME $1
+}
+
+#Check if argument is no-opengl or does not exist
+case "$1" in
+  "no-opengl")
+    echo "==== Install NVIDIA Driver $VERSION without OpenGL Files ==="
+    InstallProcess --no-opengl-files
+    ;;
+  *)
+    echo "==== Install NVIDIA Driver $VERSION ==="
+    InstallProcess
+    ;;
+esac
