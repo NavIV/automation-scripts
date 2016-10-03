78b8c7346dbf8814aeda1db8962d8c9c1e4803d4
h_ohta <h_ohta@ertl.jp>
2016-10-03 20:26:45 +0900

    Stop to use "-a" arguments of "tee" command

--- install-nvidia-driver.sh
+++ install-nvidia-driver.sh
@@ -5,9 +5,9 @@ readonly FILEURL="http://jp.download.nvidia.com/XFree86/Linux-x86_64/367.35/${FI
 
 sudo apt-get remove nvidia* && sudo apt-get autoremove
 
-echo -e "blacklist nouveau\nblacklist lbm-nouveau\noptions nouveau modeset=0\nalias nouveau off\nalias lbm-nouveau off" | sudo tee -a /etc/modprobe.d/blacklist-nouveau.conf > /dev/null
+echo -e "blacklist nouveau\nblacklist lbm-nouveau\noptions nouveau modeset=0\nalias nouveau off\nalias lbm-nouveau off" | sudo tee /etc/modprobe.d/blacklist-nouveau.conf > /dev/null
 
-echo options nouveau modeset=0 | sudo tee -a /etc/modprobe.d/nouveau-kms.conf > /dev/null
+echo options nouveau modeset=0 | sudo tee /etc/modprobe.d/nouveau-kms.conf > /dev/null
 
 sudo update-initramfs -u
 
@@ -17,7 +17,3 @@ cd $HOME/Downloads
 wget -nc $FILEURL
 chmod +x $FILENAME
 sudo sh $FILENAME
-
-
-
-
