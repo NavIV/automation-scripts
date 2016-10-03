57cb55d84350c64470ae0212e3eb6363de1eb5ef
academy01 <academy01@xxx.com>
2016-10-03 11:16:39 -0400

    Fix the problem that the commant 'catkin_make' is not found

--- install-autoware.sh
+++ install-autoware.sh
@@ -6,11 +6,10 @@
 sudo apt-get -y install ros-indigo-desktop-full ros-indigo-nmea-msgs ros-indigo-nmea-navsat-driver ros-indigo-sound-play ros-indigo-jsk-visualization
 sudo apt-get -y install libnlopt-dev freeglut3-dev qtbase5-dev libqt5opengl5-dev libssh2-1-dev libarmadillo-dev libpcap-dev gksu
 
-source ~/.bashrc
-
 cd $HOME
 git clone https://github.com/CPFL/Autoware.git
 cd ~/Autoware/ros/src
+source /opt/ros/indigo/setup.bash
 catkin_init_workspace
 cd ../
 ./catkin_make_release
